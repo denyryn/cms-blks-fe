c630681b4c479a9fce4a2642a1c88ad45d6aeab7
Deny Rian <denyryn@outlook.com>
2025-09-21 19:26:58 +0700

    add table

---
+++ src/pages/dashboard/columns.jsx
@@ -0,0 +1,71 @@
+import { Circle, EllipsisVertical } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+
+export const columns = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+      return (
+        <div className="flex items-center px-1 py-0.5 w-fit border rounded-md gap-x-1">
+          <Circle
+            className={cn({
+              "size-2 shrink-0 text-background": true,
+              "fill-green-500": status.toLowerCase() === "active",
+              "fill-red-500": status.toLowerCase() === "inactive",
+            })}
+          />
+          <span className="text-xs text-start">{status}</span>
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="data-[state=open]:bg-muted text-muted-foreground flex size-8"
+              size="icon"
+            >
+              <EllipsisVertical />
+              <span className="sr-only">Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>View</DropdownMenuItem>
+            <DropdownMenuItem>Update</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,17 +1,25 @@
 import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
+import { columns } from "./columns";
 import data from "./data.json";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-      <SectionCards />
+      {/* <SectionCards />
       <div className="px-4 lg:px-6">
         <ChartAreaInteractive />
-      </div>
-      <DataTable data={data} />
+      </div> */}
+      <section className="p-6 gap-y-4 flex flex-col items-end">
+        <Button variant="outline" className="w-12">
+          <Plus className="size-4" />
+        </Button>
+        <DataTable columns={columns} data={data} />
+      </section>
     </div>
   );
 }
