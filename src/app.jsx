130c97bcf08521a70a43ec5a8df718ee4796c7c9
Deny Rian <denyryn@outlook.com>
2025-09-21 20:54:06 +0700

    add theme mode switch

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router";
+import { ThemeProvider } from "next-themes";
 import router from "@/routes";
 import { Toaster } from "sonner";
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router} />
-      <Toaster position="bottom-right" />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <RouterProvider router={router} />
+        <Toaster position="bottom-right" />
+      </ThemeProvider>
     </>
   );
 }
